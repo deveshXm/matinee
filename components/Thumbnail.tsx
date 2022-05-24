d13d8398956cabf54d9be001f7f44771f944f754
deveshXm <deveshm.cs.20@nitj.ac.in>
2022-05-24 14:13:54 +0530

    minor bugs 3.5

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -8,7 +8,7 @@ interface Props{
 
 function Thumbnail({movie}:Props) {
   return (
-    < div className={`relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-110 md:hover:border-4 md:hover:rounded-md`}>
+    < div className={`relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-110 md:hover:border-4 md:hover:rounded-md border-white border-2 rounded-md border-opacity-40 md:hover:border-opacity-100`}>
         <Image
         src={`https://image.tmdb.org/t/p/w500${
           movie.backdrop_path || movie.poster_path
